95ae99bf359b989ba02dfa56d084253f918f9434
Sameer Jagtap <jagtap.sameer00@gmail.com>
2021-02-15 21:10:38 +0530

    Added Factory Pattern

---
+++ Swift/DesignPatterns/DesignPatternsTests/FactoryPattern.swift
@@ -0,0 +1,18 @@
+import XCTest
+@testable import DesignPatterns
+
+class FactoryPattern: XCTestCase {
+    
+    func testFactoryMethodRealWorld() {
+        
+        let info = "Very important info of the presentation"
+        
+        let clientCode = ClientCode()
+        
+        /// Present info over WiFi
+        clientCode.present(info: info, with: WifiFactory())
+        
+        /// Present info over Bluetooth
+        clientCode.present(info: info, with: BluetoothFactory())
+    }
+}
